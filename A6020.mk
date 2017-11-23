d92e4f1fe49afd6ac4900fa3b947b81093209556
BlueReptile <solowingphanzer@gmail.com>
2017-11-23 15:47:21 -0200

    Vibe K5(A6020) Update

---
+++ A6020.mk
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ROM>
+  <RomName>dot_A6020</RomName>
+  <VersionName>DOT-N-v1.1-20171123-A6020-OFFICIAL</VersionName>
+  <VersionNumber type="integer">20171123</VersionNumber>
+  <DirectUrl>https://sourceforge.net/projects/dotos-ota/files/a6020/DOT-N-v1.1-20171123-A6020-OFFICIAL.zip</DirectUrl>
+  <HttpUrl>https://sourceforge.net/projects/dotos-ota/files/a6020</HttpUrl>
+  <Android>7.1.2</Android>
+  <CheckMD5>4230f19001d85a30c38766166397a39e</CheckMD5>
+  <FileSize type="integer">469321917</FileSize>
+  <Developer>BlueReptile</Developer>
+  <Changelog>
+* First Official Release!
+* Bump Dot version to v1.1
+* New Revamp in UI
+* Add Lawnchair Launcher as pre-installed
+* Added New BootAnimation
+* Fixed Blue screen on BootAnmination bug
+* Fixed FC on DotExtra-Lockscreen
+* dotOTA added(OTA system adapted for official devices)
+* Fixed dot version unknown
+* speed up lockscreen anims
+* Improve overall System animmation lag
+* Added timber as default music app
+* Display :Qspanel theme engine
+* QS tile titles visibility
+* Memory leak Optimisation
+* Three Finger Swipe for Screenshot
+* Advanced Bluetooth, Data, Wifi Toggles
+* Screenshot crop and share
+* dotpapers: New walls added
+* Pocket mode added
+* added camera sound toggle
+* Allow disabling of FC dialogs
+* QS Header Icon settings
+* and few more bug fixes
+  </Changelog>
+</ROM>
